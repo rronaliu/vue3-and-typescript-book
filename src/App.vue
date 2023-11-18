f2c21c17e1bdcd7e8dee266d5c1f56bd01bd4ef9
rronaliu <rronaliu95@gmail.com>
2023-11-18 19:29:52 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ItemsList />
+  </div>
+</template>
+
+<script lang="ts">
+import ItemsList from './components/ItemsList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ItemsList
+  }
+})
+</script>
---
+++ src/components/ItemsList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+  </ul>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ItemsList',
+  data() {
+    return {
+      items: [
+        { id: 1, name: 'Item 1' },
+        { id: 2, name: 'Item 2' }
+      ]
+    }
+  }
+})
+</script>
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 0;
+}
+</style>
